a7d601bdf4e577d2c14c94b37dd02105b2f4ad92
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-06 00:24:58 +0200

    chore: simplify annotation parser

--- src/lsa_cli_smdmrr/annotation_parser.py
+++ src/lsa_cli_smdmrr/annotation_parser.py
@@ -21,41 +21,35 @@ class AnnotationParser:
         self.extensions_map: dict[str, str] = extensions_map
 
     def parse(self, path: str) -> list[SourceFileAnnotations]:
-        if os.path.isfile(path):
-            if any(ex in path for ex in self.parser_exclude):
-                return []
+        if any(ex in path for ex in self.parser_exclude):
             return []
 
+        if os.path.isfile(path):
+            annotation: SourceFileAnnotations | None = self._parse_file(path)
+            return [annotation] if annotation else []
+
         elif os.path.isdir(path):
-            model: list[SourceFileAnnotations] = []
+            sourceFileAnnotations: list[SourceFileAnnotations] = []
             for root, dirs, files in os.walk(path):
-                if any(ex in root for ex in self.parser_exclude):
-                    continue
-                for file in files:
-                    if any(ex == file for ex in self.parser_exclude):
-                        continue
-                    try:
-                        annotation: SourceFileAnnotations = self._parse_file(
-                            os.path.join(root, file)
-                        )
-                        if annotation.annotations:
-                            model.append(annotation)
-                    except Exception as e:
-                        logger.debug(f"Error parsing file: {os.path.join(root, file)}: {e}")
-                for dir in dirs:
-                    if any(ex == dir for ex in self.parser_exclude):
-                        continue
-            return model
+                for file in files + dirs:
+                    sourceFileAnnotations.extend(self.parse(os.path.join(root, file)))
+            return sourceFileAnnotations
         else:
             raise ValueError(f"Invalid path: {path}")
 
-    def _parse_file(self, path: str) -> SourceFileAnnotations:
-        annotations: list[Annotation] = []
-        for comment in self._parse_comments(path):
-            annotation: Annotation | None = self._convert_comment_to_annotation(comment)
-            if annotation:
-                annotations.append(annotation)
-        return SourceFileAnnotations(relative_file_path=path, annotations=annotations)
+    def _parse_file(self, path: str) -> SourceFileAnnotations | None:
+        try:
+            return SourceFileAnnotations(
+                relative_file_path=path,
+                annotations=[
+                    annotation
+                    for comment in self._parse_comments(path)
+                    if (annotation := self._convert_comment_to_annotation(comment))
+                ],
+            )
+        except Exception as e:
+            logger.debug(f"Error parsing file: {path}: {e}")
+        return None
 
     def _parse_comments(self, path: str) -> list[Comment]:
         file_extension: str = os.path.splitext(path)[1].lstrip(".")
