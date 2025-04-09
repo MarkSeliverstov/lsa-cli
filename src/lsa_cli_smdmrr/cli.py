589fa28b183e076ef5b3334c87bec01ec3920de2
Mark Seliverstov <smdmrr00@gmail.com>
2025-04-09 20:30:22 +0200

    chore: remove info logging

--- src/lsa_cli_smdmrr/cli.py
+++ src/lsa_cli_smdmrr/cli.py
@@ -40,53 +40,61 @@ def _parse_and_convert(args: Namespace, config: Config) -> None:
         config.extensions_map,
     )
 
-    logger.info(f"Parsing all annotations from source code from '{args.path}'")
+    logger.debug(f"Parsing all annotations from source code from '{args.path}'")
     model: list[SourceFileAnnotations] = parser.parse(args.path)
     if not model:
-        logger.info("No annotations found...")
+        logger.debug("No annotations found...")
         return
-    logger.info(f"Found {len(model)} files with annotations")
+    logger.debug(f"Found {len(model)} files with annotations")
 
     if args.annotations:
         _export_annotations_to_json(model, config.output_annotations_file)
-        logger.info(f"Annotations saved to '{config.output_annotations_file}'")
+        logger.debug(f"Annotations saved to '{config.output_annotations_file}'")
 
-    logger.info("Converting annotations to entities")
+    logger.debug("Converting annotations to entities")
     converter: AnnotationsToEntitiesConverter = AnnotationsToEntitiesConverter(
         config.annotations_markers_map
     )
     entities: list[Entity] = converter.convert(model)
-    logger.info(f"Found {len(entities)} entities")
+    logger.debug(f"Found {len(entities)} entities")
     _export_entities_to_json(entities, config.output_entities_file)
-    logger.info(f"Entities saved to '{config.output_entities_file}'")
+    logger.debug(f"Entities saved to '{config.output_entities_file}'")
 
-    logger.info("""
-    You can use these entities/annotations to visualize them
-    on the webpage: https://markseliverstov.github.io/MFF-bachelor-work
-    """)
+    print(
+        f"Found {len(model)} files with annotations that were converted to {len(entities)} entities."
+    )
+    print(f"    - entities saved to {config.output_entities_file}")
+    if args.annotations:
+        print(f"    - annotations saved to {config.output_annotations_file}")
+    print(
+        "You can use these entities/annotations to visualize them on the webpage: https://markseliverstov.github.io/MFF-bachelor-work"
+    )
 
 
 def run() -> None:
-    parser: ArgumentParser = ArgumentParser(
-        description="Parses annotations from source code and convert them to entities."
-    )
-    parser.add_argument("path", help="Path to file or directory to parse", type=str)
-    parser.add_argument(
-        "-c",
-        "--config",
-        help="Path to the configuration file",
-        type=str,
-    )
-    parser.add_argument(
-        "-a",
-        "--annotations",
-        help="Parsed annotations will be saved to file if this flag is set",
-        action="store_true",
-    )
-    args: Namespace = parser.parse_args()
-    config: Config = (
-        Config.from_file(args.config)
-        if args.config
-        else Config.from_file(Config.DEFAULT_CONFIG_PATH)
-    )
-    _parse_and_convert(args, config)
+    try:
+        parser: ArgumentParser = ArgumentParser(
+            description="Parses annotations from source code and convert them to entities."
+        )
+        parser.add_argument("path", help="Path to file or directory to parse", type=str)
+        parser.add_argument(
+            "-c",
+            "--config",
+            help="Path to the configuration file",
+            type=str,
+        )
+        parser.add_argument(
+            "-a",
+            "--annotations",
+            help="Parsed annotations will be saved to file if this flag is set",
+            action="store_true",
+        )
+        args: Namespace = parser.parse_args()
+        config: Config = (
+            Config.from_file(args.config)
+            if args.config
+            else Config.from_file(Config.DEFAULT_CONFIG_PATH)
+        )
+        _parse_and_convert(args, config)
+    except KeyboardInterrupt:
+        pass
